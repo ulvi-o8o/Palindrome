d20801de8be4121a91d728c9e4cc88dbee2d8c2e
unknown <ulviguliyev080@gmail.com>
2024-03-14 17:08:16 +0300

    Test

--- ca-3-hw.js
+++ ca-3-hw.js
@@ -38,3 +38,35 @@ function isPalindrome(num) {
 console.log(isPalindrome(45654));
 console.log(isPalindrome(8956));
 console.log(isPalindrome(85258));
+
+// ANAGRAM
+
+function isAnagram(str1, str2) {
+  const cleanStr1 = str1.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  const cleanStr2 = str2.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+
+  //   if (cleanStr1.length !== cleanStr2.length) {
+  //     return false;
+  //   }
+
+  const sortedStr1 = cleanStr1.split("").sort().join("");
+  const sortedStr2 = cleanStr2.split("").sort().join("");
+
+  return sortedStr1 === sortedStr2;
+}
+
+console.log(isAnagram("listen", "silent"));
+console.log(isAnagram("world", "low"));
+
+// Palindrome
+
+function isPalindrome(num) {
+  const numStr = String(num);
+
+  return numStr === numStr.split("").reverse().join("");
+}
+
+console.log(isPalindrome(121)); // true
+console.log(isPalindrome(12321)); // true
+console.log(isPalindrome(12345)); // false
+console.log(isPalindrome(1213121));
